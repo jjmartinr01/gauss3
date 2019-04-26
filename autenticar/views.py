59b5e41679a2c8561ef683a9b47688d2a78db438
Juanjo <jmar0269@gmail.com>
2019-04-26 09:27:17 +0200

    permitir acceso a jjmartinr01

--- autenticar/views.py
+++ autenticar/views.py
@@ -289,7 +289,8 @@ def index(request):
                         request.session["ronda"] = request.session["gauser_extra"].ronda
                         request.session['num_items_page'] = 15
                         logger.info(u'%s se loguea en GAUSS.' % (request.session["gauser_extra"]))
-                        if request.session['gauser_extra'].entidad.id in [14, 16]:
+                        usernombre = request.session['gauser_extra'].gauser.username
+                        if request.session['gauser_extra'].entidad.id in [14, 16] and usernombre != 'jjmartinr01':
                             return render(request, "temporalmente_inactivo.html")
                         return redirect('/calendario/')
                     else:
@@ -334,7 +335,8 @@ def index(request):
                         request.session["ronda"] = request.session["gauser_extra"].ronda
                         request.session['num_items_page'] = 15
                         logger.info(u'%s se loguea en GAUSS.' % (request.session["gauser_extra"]))
-                        if request.session['gauser_extra'].entidad.id in [14, 16]:
+                        usernombre = request.session['gauser_extra'].gauser.username
+                        if request.session['gauser_extra'].entidad.id in [14, 16] and usernombre != 'jjmartinr01':
                             return render(request, "temporalmente_inactivo.html")
                         return redirect('/calendario/')
                     else:
@@ -385,7 +387,8 @@ def index(request):
             request.session["ronda"] = request.session["gauser_extra"].ronda
             request.session['num_items_page'] = 15
             logger.info(u'%s se loguea en GAUSS.' % (request.session["gauser_extra"]))
-            if request.session['gauser_extra'].entidad.id in [14, 16]:
+            usernombre = request.session['gauser_extra'].gauser.username
+            if request.session['gauser_extra'].entidad.id in [14, 16] and usernombre != 'jjmartinr01':
                 return render(request, "temporalmente_inactivo.html")
             return redirect('/calendario/')
 
