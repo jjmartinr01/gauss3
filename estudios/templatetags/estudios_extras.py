4c4e8a6963bfa2d384ec0565cad54797dcd50c6d
Juanjo <jmar0269@gmail.com>
2019-11-22 16:15:00 +0100

    Arreglos en carta de materias pendientes

--- estudios/templatetags/estudios_extras.py
+++ estudios/templatetags/estudios_extras.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.template import Library
+from gauss.funciones import human_readable_list
 from entidades.models import Subentidad
-from estudios.models import Curso, Grupo, Materia, Gauser_extra_estudios
+from estudios.models import Curso, Grupo, Materia, Gauser_extra_estudios, Matricula
 from datetime import datetime, time
 
 register = Library()
@@ -20,3 +21,15 @@ def materias_grupo(grupo):
 @register.filter
 def alumnos_grupo(grupo):
     return Gauser_extra_estudios.objects.filter(grupo=grupo)
+
+@register.filter
+def pendientes_alumno(ms, alumno):
+    return Matricula.objects.filter(ge=alumno, materia__in=ms)
+
+@register.filter
+def human_readable_pendientes(matriculas):
+    materias = matriculas.values_list('id', flat=True)
+    ms = Materia.objects.filter(id__in=materias)
+    ms_text_array = ['%s (%s)' % (m[0], m[1]) for m in ms.values_list('nombre', 'curso__nombre')]
+    return human_readable_list(ms_text_array)
+
