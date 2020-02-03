15fde549c1c627aeb7808cfb9cb2b964aa74518e
Juanjo <jmar0269@gmail.com>
2020-02-03 13:44:55 +0100

    Ges documental completamente renovado 3

--- documentos/urls.py
+++ documentos/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^documentos/$', views.documentos, name='documentos'),
-    url(r'^documentos_ajax/$', views.documentos_ajax, name='documentos_ajax'),
+    # url(r'^documentos_ajax/$', views.documentos_ajax, name='documentos_ajax'),
     url(r'^contrato_gauss/$', views.contrato_gauss, name='contrato_gauss'),
     url(r'^presentaciones/$', views.presentaciones, name='presentaciones'),
 ]
