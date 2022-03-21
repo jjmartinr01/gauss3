abaeb9e96ae0ac011242d09ffeac8a4956667cc8
Juanjo <jmar0269@gmail.com>
2022-03-21 08:56:03 +0100

    Programaciones y Cuaderno profesor 28

--- programaciones/models.py
+++ programaciones/models.py
@@ -756,7 +756,7 @@ class SaberBas(models.Model):
     orden = models.IntegerField('Orden del saber básico dentro del conjunto de saberes', default=1)
     # parent = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
     nombre = models.CharField('Nombre de la actividad', blank=True, max_length=300)
-    comienzo = models.DateField('Fecha de comienzo programada', default=now)
+    # comienzo = models.DateField('Fecha de comienzo programada', default=now)
     periodos = models.IntegerField('Número estimado de periodos lectivos para impartirlo', default=1)
     librorecursos = models.ManyToManyField(LibroRecurso, blank=True)
     actexcoms = models.ManyToManyField(ActExCom, blank=True)
@@ -790,7 +790,8 @@ class SaberBas(models.Model):
         # return 1 if num == 0 else num
 
     class Meta:
-        ordering = ['psec', 'comienzo', 'orden']
+        # ordering = ['psec', 'comienzo', 'orden']
+        ordering = ['psec', 'orden']
 
     def __str__(self):
         return '%s - %s (%s)' % (self.psec, self.nombre, self.periodos)
