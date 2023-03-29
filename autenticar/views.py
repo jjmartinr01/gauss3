22f602931c41cc06c89a3ca04a0103ebee0142cd
Juanjo <jmar0269@gmail.com>
2023-03-29 21:04:37 +0200

    Arreglar cargas masivas 2

--- autenticar/views.py
+++ autenticar/views.py
@@ -1009,6 +1009,8 @@ def carga_masiva(request):
                             'PERSONAL_CENTROS': 'carga_personal_centros_educativos',
                             'DATOS_CENTROS': 'carga_datos_centros_educativos',
                             'HORARIO_PERSONAL_CENTRO': 'carga_horario_personal_centro_educativo',
+                            'HORARIO_PERSONAL_CENTROS': 'carga_horario_personal_centros_educativos',
+                            'DATOS_CASIOPEA': 'carga_plantilla_organica_casiopea',
                             }
             if not g_e.has_permiso(tipo2permiso[request.POST['select_tipo_carga']]):
                 return render(request, "enlazar.html", {'page': '/', })
