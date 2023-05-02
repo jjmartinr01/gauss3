85ecb21811a5ee2c09bbf9b2f27acd7e001cce9b
Espe <emartinez@iescomercio.com>
2023-05-02 18:59:21 +0200

    Ckeditor custom toolbars

--- static_files/ckeditor/config.js
+++ static_files/ckeditor/config.js
@@ -7,4 +7,15 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    // The toolbar groups arrangement, optimized for one toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		{ name: 'styles' },
+		{ name: 'colors' }
+	];
+    // Remove some buttons, which we don't need to have in the custom toolbar.
+	config.removeButtons = 'Strike,Blockquote,CreateDiv';
 };
