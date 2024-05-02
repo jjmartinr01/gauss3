97b3375cb314ba05ce85daddcc3be6211eb47204
John Doe <johndoe@example.com>
2024-05-02 16:29:03 +0200

    [16] Change cleanMigration right path, fix warning compose

--- scripts/cleanMigration.sh
+++ scripts/cleanMigration.sh
@@ -1,10 +1,10 @@
 # Borramos las migraciones de las app del core de Django
-find ./usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*.pyc" -delete
 
 # Borramos las migraciones de las app de Gauss
-#find ../ -path "*/migrations/*.py" -not -name "__init__.py" -delete
-#find ../ -path "*/migrations/*.pyc" -delete
+find /var/www/python/gauss3 -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find /var/www/python/gauss3 -path "*/migrations/*.pyc" -delete
 
 for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage
 do
