f4fc523be03ccb1f851188ab2a32777935367b91
ProfVespertino <ProfVespertino@PROFESOR14.informatica.comercio>
2024-05-02 18:41:12 +0200

    [16] Modify script root path script, fix migrations

--- scripts/cleanMigration.sh
+++ scripts/cleanMigration.sh
@@ -6,15 +6,15 @@ find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*
 find /var/www/python/gauss3 -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find /var/www/python/gauss3 -path "*/migrations/*.pyc" -delete
 
-for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage
+for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs
 do
-    rm -Rf ./$app/migrations/
+    rm -Rf /var/www/python/gauss3/$app/migrations/
 done
 
 # Generamos
-for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage
+for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs
 do
-    python manage.py makemigrations $app
+    python /var/www/python/gauss3/manage.py makemigrations $app
 done
 
-python manage.py migrate --fake-initial
+python /var/www/python/gauss3/manage.py migrate --fake-initial
---
+++ scripts/re_migrations.sh
@@ -0,0 +1,20 @@
+# Borramos las migraciones de las app del core de Django
+find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*.pyc" -delete
+
+# Borramos las migraciones de las app de Gauss
+find /var/www/python/gauss3 -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find /var/www/python/gauss3 -path "*/migrations/*.pyc" -delete
+
+for app in bancos autenticar entidades mensajes calenario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
+do
+    rm -Rf /var/www/python/gauss3/$app/migrations/ 
+done
+
+# Generamos
+for app in bancos autenticar entidades mensajes calenario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
+do
+    python /var/www/python/gauss3/manage.py makemigrations $app
+done
+
+python /var/www/python/gauss3/manage.py migrate --fake-initial
