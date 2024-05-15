341af8cba82c4293aab5e11322ef01e2fd566437
John Doe <johndoe@example.com>
2024-05-15 16:22:03 +0200

    [16] Modify docker compose allow Migrations

--- scripts/re_migrations.sh
+++ scripts/re_migrations.sh
@@ -6,13 +6,13 @@ find /usr/local/lib/python3.7/site-packages/django/contrib -path "*/migrations/*
 find /var/www/python/gauss3 -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find /var/www/python/gauss3 -path "*/migrations/*.pyc" -delete
 
-for app in bancos autenticar entidades mensajes calenario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
+for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
 do
     rm -Rf /var/www/python/gauss3/$app/migrations/ 
 done
 
 # Generamos
-for app in bancos autenticar entidades mensajes calenario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones webpage moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
+for app in bancos autenticar entidades mensajes calendario contabilidad actas documentos lopd vestuario apariencia compraventa web formularios gtelegram horarios cupo convivencia absentismo registro reparaciones actividades tutorados programaciones estudios competencias_clave vut domotica reuniones moscosos inspeccion_educativa federaciones faqs admin auth contenttypes sessions sites
 do
     python /var/www/python/gauss3/manage.py makemigrations $app
 done
