e5e796771b6c774286248f67fce129742d42f100
John Doe <johndoe@example.com>
2024-05-23 22:45:11 +0200

    [16] Modify docker compose missing log file error

--- scripts/django_start.sh
+++ scripts/django_start.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-touch /var/www/python/gauss3/log/logGaussDomotica.log 
 python /var/www/python/gauss3/manage.py collectstatic --noinput
 
 exec /usr/local/bin/gunicorn gauss.wsgi -w 3 --threads 3 --bind 0.0.0.0:8000 --chdir=/var/www/python/gauss3 
