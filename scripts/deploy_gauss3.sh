5dea4da47a4f0e2423f3671d6f0ed7a06788a35d
rjalamanac <rjalamanac@iescomercio.com>
2024-06-19 17:02:25 +0200

    [54] change deploy gauss script

--- scripts/deploy_gauss3.sh
+++ scripts/deploy_gauss3.sh
@@ -3,7 +3,7 @@
 # Define variables
 REPO_URL="https://github.com/jjmartinr01/gauss3"
 BRANCH="develop"
-LOCAL_DIR="/var/www/python/gauss3/"
+LOCAL_DIR="$(dirname "$(realpath "$0")")"  #the parent directory
 DOCKER_COMPOSE_FILE="$LOCAL_DIR/docker-compose.yml"
 LOG_FILE="/var/log/deploy_gauss3.log"
 
@@ -16,23 +16,17 @@ fi
 # Log the start of the deployment
 echo "$(date): Starting deployment" | tee -a $LOG_FILE
 
-# Pull the latest code from the repository
-if [ ! -d "$LOCAL_DIR" ]; then
-  git clone -b $BRANCH $REPO_URL $LOCAL_DIR | tee -a $LOG_FILE
-else
-  cd $LOCAL_DIR
-  git fetch origin | tee -a $LOG_FILE
-  git pull origin/$BRANCH | tee -a $LOG_FILE
-fi
 
-# Navigate to the project directory
 cd $LOCAL_DIR
+git fetch origin | tee -a $LOG_FILE
+git pull origin/$BRANCH | tee -a $LOG_FILE
+
 
 # Build and deploy the Docker Compose setup
-docker-compose -f $DOCKER_COMPOSE_FILE down | tee -a $LOG_FILE
-docker-compose -f $DOCKER_COMPOSE_FILE pull | tee -a $LOG_FILE
-docker-compose -f $DOCKER_COMPOSE_FILE build | tee -a $LOG_FILE
-docker-compose -f $DOCKER_COMPOSE_FILE up -d --remove-orphans | tee -a $LOG_FILE
+docker compose -f $DOCKER_COMPOSE_FILE down | tee -a $LOG_FILE
+docker compose -f $DOCKER_COMPOSE_FILE pull | tee -a $LOG_FILE
+docker compose -f $DOCKER_COMPOSE_FILE build | tee -a $LOG_FILE
+docker compose -f $DOCKER_COMPOSE_FILE up -d --remove-orphans | tee -a $LOG_FILE
 
 # Log the completion of the deployment
 echo "$(date): Deployment completed" | tee -a $LOG_FILE
